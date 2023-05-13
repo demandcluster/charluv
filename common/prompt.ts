769258f6293c767aa507ccca455532142ef87a10
Ron W. van Etten <rondlite@gmail.com>
2023-05-13 21:34:16 +0200

    persoan fix2

--- common/prompt.ts
+++ common/prompt.ts
@@ -271,7 +271,7 @@ export function formatCharacter(
   switch (kind || persona.kind) {
     case 'wpp': {
       const attrs = Object.entries(persona.attributes)
-        .map(([key, values]) => `${key}(${values.map(quote).join(' + ')})`)
+        .map(([key, values]) => `${key}(${values?.map(quote).join(' + ') || ''})`)
         .join('\n')
 
       return [`[character("${name}") {`, attrs, '}]'].join('\n')
