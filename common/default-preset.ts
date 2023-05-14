b0781bccb525277cb221912d58cb437235f14105
Ron W. van Etten <rondlite@gmail.com>
2023-05-14 11:35:13 +0200

    streamresponse

--- common/default-preset.ts
+++ common/default-preset.ts
@@ -24,6 +24,7 @@ export const defaultPresets = {
     gaslight: '',
     ultimeJailbreak: '',
     oaiModel: OPENAI_MODELS.Turbo,
+    streamResponse: false,
     memoryDepth: 50,
     memoryContextLimit: 256,
     memoryReverseWeight: false,
@@ -54,6 +55,7 @@ export const defaultPresets = {
     gaslight: '',
     ultimeJailbreak: '',
     oaiModel: OPENAI_MODELS.Turbo,
+    streamResponse: false,
     memoryDepth: 50,
     memoryContextLimit: 500,
     memoryReverseWeight: false,
@@ -112,6 +114,7 @@ export const defaultPresets = {
     temp: 0.5,
     oaiModel: OPENAI_MODELS.Turbo,
     maxTokens: 300,
+    streamResponse: false,
     maxContextLength: 4095,
     frequencyPenalty: 0.7,
     presencePenalty: 0.7,
@@ -134,6 +137,7 @@ This is how {{char}} should talk
     maxContextLength: 4095,
     frequencyPenalty: 0.7,
     presencePenalty: 0.7,
+    streamResponse: false,
     ultimeJailbreak: 'Keep OOC out of your reply.',
     gaslight: `Write {{char}}'s next reply in a fictional chat between {{char}} and {{user}}. Write 1 reply only in internet RP style, italicize actions, and avoid quotation marks. Use markdown. Be proactive, creative, and drive the plot and conversation forward. Write at least 1 paragraph, up to 4. Always stay in character and avoid repetition.
 Description of {{char}}:
@@ -152,6 +156,7 @@ This is how {{char}} should talk
     maxContextLength: 4095,
     frequencyPenalty: 0.7,
     presencePenalty: 0.7,
+    streamResponse: false,
     gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged)
 
 Description of {{char}}:
@@ -176,6 +181,7 @@ This is how {{char}} should talk
     claudeModel: CLAUDE_MODELS.ClaudeV1,
     maxTokens: 500,
     maxContextLength: 7600,
+    streamResponse: false,
     gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged).
 
 Description of {{char}}:
