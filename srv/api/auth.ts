82d6df15f5ace8635c177267615421bdb17776b9
Ron W. van Etten <rondlite@gmail.com>
2023-05-16 17:46:09 +0200

    mayube

--- srv/api/auth.ts
+++ srv/api/auth.ts
@@ -4,7 +4,7 @@ import { AppRequest, errors } from './wrap'
 import { config } from '../config'
 
 export const authMiddleware: any = (req: AppRequest, _res: Response, next: NextFunction) => {
-  const socketId = req.get('socket-id') || req.get('socketId') || ''
+  const socketId = req.get('socket-id') || ''
   req.socketId = socketId
 
   const header = req.get('authorization')
