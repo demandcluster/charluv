1496f29cf2c49ea80cf8424e5d6b7d6c8897de4b
Ron van Etten <rondlite@gmail.com>
2024-09-21 02:55:05 +0200

    image

--- web/store/data/image.ts
+++ web/store/data/image.ts
@@ -257,9 +257,11 @@ function getSummaryTemplate(service: AIAdapter) {
       {{#each msg}}{{#if .isbot}}### Response:\n{{.name}}: {{.msg}}{{/if}}{{#if .isuser}}### Instruction:\n{{.name}}: {{.msg}}{{/if}}
       {{/each}}
 
+
+
       ### Instruction:
       Write an image caption of the current scene using physical descriptions without names. Write a summary of facts based on the conversation.
-      Do not include level or any charluv related information. Only include information that is relevant to the conversation. No relationship level, just pure facts about the conversation.
+      Do not include level or any charluv related information. Only include information that is relevant to the conversation. No relationship level, just pure facts about the conversation and a detailed description of {{char}}'s apppearance.
       
       ### Response:
       Image caption: [summary | tokens=200]
