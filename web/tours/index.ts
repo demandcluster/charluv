d83971dfd998a1b43c8c8d5926bc6e424a09f169
rondlite <rondlite@gmail.com>
2024-10-19 04:09:47 +0200

    Update index.ts

--- web/tours/index.ts
+++ web/tours/index.ts
@@ -25,6 +25,7 @@ export function canStartTour(type: TourType, force?: boolean) {
   const forceTours = force || getStoredValue(`force-tours`, false)
 
   if (isLoggedIn() && !forceTours) return false
+  if (!isLoggedIn()) return false
   if (isComplete && !force) return false
   if (tour.isActive()) return false
 
