69fbb8925b5c9dcbe67287372625f256c0a7807c
rondlite <rondlite@gmail.com>
2026-03-08 22:28:40 +0100

    Change default model from 'Deliberate' to 'Anygen'

--- common/horde-gen.ts
+++ common/horde-gen.ts
@@ -13,7 +13,7 @@ const baseUrl = 'https://horde.aivo.chat/api/v2'
 export const defaults = {
   image: {
     sampler: SD_SAMPLER['DPM++ 2M'],
-    model: 'Deliberate',
+    model: 'Anygen',
     negative: ``,
   },
 }
